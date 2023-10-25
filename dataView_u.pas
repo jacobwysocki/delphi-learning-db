f8376079700300ed593c18d7595819ac644292eb
Jakub <jwysocki@mandata.co.uk>
2023-10-25 14:10:25 +0100

    Added insert record button that works with the db

--- dataView_u.pas
+++ dataView_u.pas
@@ -14,16 +14,21 @@ type
     btnSort: TButton;
     btnFindRecord: TButton;
     edtFind: TEdit;
-    DBGrid1: TDBGrid;
     btnFindAverage: TButton;
-    edtFindAverage: TEdit;
     btnSortPriceAsc: TButton;
     btnSortPriceDesc: TButton;
+    Panel1: TPanel;
+    lblAveragePrice: TLabel;
+    btnInsertRecord: TButton;
+    btnEditRecord: TButton;
+    btnDeleteRecord: TButton;
+    DBGrid1: TDBGrid;
     procedure btnSortClick(Sender: TObject);
     procedure btnFindRecordClick(Sender: TObject);
     procedure btnSortPriceAscClick(Sender: TObject);
     procedure btnSortPriceDescClick(Sender: TObject);
     procedure btnFindAverageClick(Sender: TObject);
+    procedure btnInsertRecordClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -91,8 +96,38 @@ begin
   rAverage := rSum / tblCD.RecordCount;
 
   ShowMessage('Average Price is: £' + FloatToStr(rAverage) + '. ' + 'Total Cost of all CDs is: £' + FloatToStr(rSum) + '. ');
+  lblAveragePrice.Caption := '£' + FloatToStr(rAverage);
 end;
 
 end;
+
+procedure TdataView.btnInsertRecordClick(Sender: TObject);
+var sArtist, sCDName, sGenre : string;
+    iPrice, iID, iOwnerID : integer;
+begin
+    sArtist := Inputbox('Artist', 'Enter Artist name: ', '49 Cent');
+    sCDName := InputBox('CD Name', 'Enter CD Name: ', 'Life of a Cent');
+    sGenre := InputBox('Genre', 'Enter Genre of the Music: ', 'Hip Hop');
+    iPrice := StrToInt(InputBox('Price', 'Enter the Price: ', '14'));
+    iOwnerID := StrToInt(InputBox('Owner', 'Enter owners ID: ', '1'));
+
+    dmCD.tblCD.Sort := 'ID ASC';
+    dmCD.tblCD.Last;
+
+    //If ID is not set as an AutoNumbr
+    //iID := dmCD.tblCD['ID'] + 1;
+
+    begin
+      dmCD.tblCD.Insert;
+      //dmCD.tblCD['ID'] := iID;
+      dmCD.tblCD['Artist'] := sArtist;
+      dmCD.tblCD['CD_Name'] := sCDName;
+      dmCD.tblCD['Genre'] := sGenre;
+      dmCD.tblCD['Price'] := iPrice;
+      dmCD.tblCD['OwnerID'] := iOwnerID;
+      dmCD.tblCD.Post;
+    end;
+end;
+
 
 end.
